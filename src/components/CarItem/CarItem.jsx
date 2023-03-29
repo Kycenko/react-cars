3788ad7b6ec11c7b0073b3633a5b25d4754cd421
Vladislav <lagunovich.vladislav2003@gmail.com>
2023-03-29 11:10:14 +0300

    addedRedux

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './CarItem.module.scss'
-import MyButton from "../UI/button/MyButton.jsx";
+
 import {Link} from "react-router-dom";
 
 const CarItem = ({car}) => {
